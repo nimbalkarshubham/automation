9e531372d99d7ecd2a5555bbd6e6a9c9d83150a9
mindsdet279 <mindsdet279@mindc1d389.makemylabs.in>
2022-05-23 14:22:33 +0530

    first commit

---
+++ compressive/src/main/java/com/mindtree/compressive/Testcase2.java
@@ -0,0 +1,29 @@
+package com.mindtree.compressive;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testcase2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\browsers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.urbanladder.com/");
+		driver.findElement(By.xpath("//span[@class='header-icon-link user-profile-icon']")).click();
+		driver.findElement(By.xpath("//a[@class='signup-link authentication_popup']")).click();
+		
+		Thread.sleep(5000);
+		driver.findElement(By.cssSelector("#spree_user_email")).click();
+		driver.findElement(By.cssSelector("#spree_user_email")).sendKeys("shubhamnimbalkar@gmail.com");
+		driver.findElement(By.xpath("(//input[@id='spree_user_password'])[2]")).click();
+		driver.findElement(By.xpath("(//input[@id='spree_user_password'])[2]")).sendKeys("shubham54321");
+		
+		driver.findElement(By.xpath("(//input[@name='commit'])[2]")).click();
+		Thread.sleep(2000);
+		driver.close();
+
+	}
+
+}
---
+++ compressive/src/main/java/com/mindtree/compressive/testcase1.java
@@ -0,0 +1,30 @@
+package com.mindtree.compressive;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testcase1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\browsers\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.urbanladder.com/");
+		
+		driver.findElement(By.xpath("//span[@class='header-icon-link user-profile-icon']")).click();
+		driver.findElement(By.xpath("//a[@class='login-link authentication_popup']")).click();
+		
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("(//input[@id='spree_user_email'])[2]")).click();
+		driver.findElement(By.xpath("(//input[@id='spree_user_email'])[2]")).sendKeys("Shubhamnimbalkar12@gmail.com");
+		driver.findElement(By.xpath("(//input[@id='spree_user_password'])[3]")).click();
+		driver.findElement(By.xpath("(//input[@id='spree_user_password'])[3]")).sendKeys("Shubham@54321");
+		driver.findElement(By.xpath("//input[@id='ul_site_login']")).click();
+		
+		Thread.sleep(2000);
+		driver.close();
+
+
+	}
+
+}
